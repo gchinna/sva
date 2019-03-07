5e1763bbc8c752d2ae8d2b28bb291f9dcaebf71f
Ganesan Chinnathambi <ganesancp@gmail.com>
2019-03-07 08:40:27 -0800

    Update assert_macros.svh
    add ASSERT_SVA_RPT compile-time option.

--- assert_macros.svh
+++ assert_macros.svh
@@ -1,13 +1,20 @@
 `ifndef ASSERT_MACROS_SVH
 `define ASSERT_MACROS_SVH
 
+// assert macros use UVM reporting for errors by default.
+// use +define+ASSERT_SVA_RPT to use SVA $error() for reporting errors.
+
 // assert property (PROP) on sampling event @(posedge clk)
 `define assert_clk( PROP, CLK=clk, RPT_ID=report_id, MSG="assertion failed!" )  \
     assert property (  \
         @(posedge CLK)  \
         (PROP) )  \
     else begin  \
-        `uvm_error(RPT_ID, $sformatf("%s:%0d : %s", `__FILE__, `__LINE__, MSG))  \
+        `ifdef ASSERT_SVA_RPT  \
+            $error($sformatf("[%s] %s", RPT_ID, MSG))  \
+        `else  \
+            `uvm_error(RPT_ID, $sformatf("%s", MSG))  \
+        `endif  \
     end // assert
     
 // assert property (PROP) on sampling event @(posedge clk) except when rst_n is asserted
@@ -17,7 +24,10 @@
         disable iff(!RST_N)  \
         (PROP) )  \
     else begin  \
-        `uvm_error(RPT_ID, $sformatf("%s:%0d : %s", `__FILE__, `__LINE__, MSG))  \
+        `ifdef ASSERT_SVA_RPT  \
+            $error($sformatf("[%s] %s", RPT_ID, MSG))  \
+        `else  \
+            `uvm_error(RPT_ID, $sformatf("%s", MSG))  \
     end // assert
-        
+ 
 `endif // ASSERT_MACROS_SVH
